756ba94f9b216ab3514fdcec0b72dce866c213fa
moolina01 <mbmoolina@gmail.com>
2025-11-29 13:52:37 -0300

    fix: disable prerender for confirmacion page

--- app/confirmacion/page.tsx
+++ app/confirmacion/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useEffect, useState, Suspense } from "react";
 import { supabase } from "@/utils/supabase";
 import { useSearchParams, useRouter } from "next/navigation";

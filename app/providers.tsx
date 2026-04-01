2d668b7468eb0c83f279f068709194aebbdf4d34
moolina01 <mbmoolina@gmail.com>
2026-04-01 10:58:28 -0300

    fix PostHog env var name to NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,8 +6,8 @@ import { useEffect } from "react";
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://app.posthog.com",
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
       capture_pageview: true,
       capture_pageleave: true,
     });
